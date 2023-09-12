ea8e840d7c8f627edb609ea4ce8402e9b849d63d
Avishkar Sabnis <32098673+avishkar-sabnis@users.noreply.github.com>
2023-09-12 16:25:54 +0530

    Retry Implemented for EmployeeService.java in getEmployeeById

--- EmployeeService/src/main/java/com/EmployeeManagement/EmployeeService/EmployeeController.java
+++ EmployeeService/src/main/java/com/EmployeeManagement/EmployeeService/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.EmployeeManagement.EmployeeService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,34 +22,40 @@ public class EmployeeController {
         return employeeService.createEmployee(employee);
     }
 
+
     @GetMapping("/ShowALlEmployees")
+
     public Iterable<Employee> getAllEmployees(){
+
         return employeeService.getAllEmployees();
     }
 
+    int retryCount = 1;
     @GetMapping("/ShowEmployeeById/{employeeId}")
-    @CircuitBreaker(name = "employeeBreak" ,fallbackMethod = "fallbackIncaseOfFail")
+    //@CircuitBreaker(name = "employeeBreak" ,fallbackMethod = "fallbackIncaseOfFail")
+    @Retry(name = "employeeBreakRetry",fallbackMethod = "fallbackIncaseOfRetry")
     public Employee getEmployeeById(@PathVariable int employeeId){
+        System.out.println(retryCount);
+        retryCount++;
         return employeeService.getEmployeeById(employeeId);
     }
 
 
-    public Employee fallbackIncaseOfFail(Exception e){
+    /*public Employee fallbackIncaseOfFail(Exception e){
+
         return new Employee(0,"NullAddress",0,0,"EmployeeNotExist");
+    }*/
+
+    public Employee fallbackIncaseOfRetry(Exception e){
+
+        return  new Employee(0,"NullAddress",0,0,"EmployeeNotExist");
     }
-
-
-
 
 
     @GetMapping("/getEmployeeNameById/{employeeId}")
     public String getEmployeeNameById(@PathVariable int employeeId){
         return employeeService.getEmployeeNameById(employeeId);
     }
-
-
-
-
 
     @PutMapping("/updateEmployeeDetails/{employeeId}")
     public Employee updateEmployeeDetails(@RequestBody Employee newEmployeeData, @PathVariable int employeeId){
